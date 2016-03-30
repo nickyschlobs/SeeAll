35c581d19db85b4041705600dbf373e5e938c7bc
Nick Schlobohm <nickyschlobs@gmail.com>
2016-03-30 16:56:50 +1000

    Updated code formatting style to Zend Framework 1 conventions

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
 // for example:
 // define ( 'VENDOR_DIR', '/var/seeall/vendor' );
 // define ( 'IMAGES_DIR', '/var/seeall/images' );
-$_CONFIG = array (
-		'db' => array (
-				'db' => 'seeall',
-				'host' => 'localhost',
-				'user' => 'root',
-				'pass' => '',
-				'port' => 3306 
-		) 
+$_CONFIG = array(
+        'db' => array(
+                'db' => 'seeall',
+                'host' => 'localhost',
+                'user' => 'root',
+                'pass' => '',
+                'port' => 3306
+        )
 );
--- seeall.php
+++ seeall.php
@@ -1,8 +1,12 @@
 <?php
-require_once (dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'config.php');
+require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config.php');
 
-if (! defined ( 'VENDOR_DIR' ))
-	define ( 'VENDOR_DIR', dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'vendor' );
+if (! defined('VENDOR_DIR')) {
+    define('VENDOR_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor');
+}
 
-if (! defined ( 'IMAGES_DIR' ))
-	define ( 'IMAGES_DIR', dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'images' );
+if (! defined('IMAGES_DIR')) {
+    define('IMAGES_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'images');
+}
+
+
